57e2dbe4a708f15a7259a3115aecf2a16d685172
Thabang Maphangisane <thabangh647@gmail.com>
2023-12-05 01:26:59 +0200

    fix: 14. CPython #0: Python lists - headers and variables

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -3,15 +3,16 @@
 #include <listobject.h>
 
 /**
- * print_python_list_info - print python list
- * @p: python object
- * Return: None.
+ * print_python_list_info - prints some basic info about
+ * Python lists.
+ *
+ * @p: PyObject
+ * Return: no return
  */
-
 void print_python_list_info(PyObject *p)
 {
-	long int size = PyList_Size(p);
 	int i;
+	long int size = PyList_Size(p);
 	PyListObject *obj = (PyListObject *)p;
 
 	printf("[*] Size of the Python List = %li\n", size);
@@ -19,6 +20,7 @@ void print_python_list_info(PyObject *p)
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+		item = obj->ob_item[i];
+		printf("Element %i: %s\n", i, Py_TYPE(item)->tp_name);
 	}
 }
