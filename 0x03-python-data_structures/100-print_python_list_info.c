c1f4064f1dea9bbe3f4efa75e1113e5ab766b900
Thabang Maphangisane <thabangh647@gmail.com>
2023-12-05 01:43:20 +0200

    fix: 14. CPython #0: Python lists - remove item var

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -20,7 +20,6 @@ void print_python_list_info(PyObject *p)
 
 	for (i = 0; i < size; i++)
 	{
-		item = obj->ob_item[i];
-		printf("Element %i: %s\n", i, Py_TYPE(item)->tp_name);
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
 	}
 }
