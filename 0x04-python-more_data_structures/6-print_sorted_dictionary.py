7afab63c8afc17aed91c03ba62f24e405eed41b6
Thabang Maphangisane <thabangh647@gmail.com>
2023-12-06 15:02:59 +0200

    6. Print sorted dictionary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sorted_list = list(a_dictionary.keys())
+    sorted_list.sort()
+    for i in sorted_list:
+        print("{}: {}".format(i, a_dictionary.get(i)))
