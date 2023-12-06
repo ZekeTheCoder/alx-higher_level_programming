9be363f9381c10ad57e3e320b8c132bee44b84bf
Thabang Maphangisane <thabangh647@gmail.com>
2023-12-06 15:24:34 +0200

    refactor: 9. Multiply by 2 - removing use of list creation and for loop.

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = a_dictionary.copy()
-    list_keys = list(new_dict.keys())
-    for i in list_keys:
-        new_dict[i] *= 2
-    return (new_dict)
+    new_dict = {key: value * 2 for key, value in a_dictionary.items()}
+    return new_dict
