ee414395a387a6d70c8cf85d8931c4ac9499609f
Thabang Maphangisane <thabangh647@gmail.com>
2023-12-06 18:22:56 +0200

    fix: 15. Delete by value - using list comprehension

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    keys_to_del = []
+    keys_to_keep = [key for key, val in a_dictionary.items() if val != value]
+    new_dict = {key: a_dictionary[key] for key in keys_to_keep}
 
-    for key in a_dictionary:
-        if a_dictionary[key] == value:
-            keys_to_del.append(key)
-    
-    for key in keys_to_del:
-        del a_dictionary[key]
-
-    return (a_dictionary)
+    return (new_dict)
