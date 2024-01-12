0eb7556e93174ea1c46665644f749697d31f0702
Thabang Maphangisane <thabangh647@gmail.com>
2024-01-12 15:06:01 +0200

    4. Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""4. Text indentation"""
+
+
+def text_indentation(text):
+    """Print text with two new lines after each '.', '?', and ':'.
+
+    Args:
+        text (string): The text to print.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    txt = text[:]
+
+    for char in ".?:":
+        list_text = txt.split(char)
+        txt = ""
+
+        for element in list_text:
+            element = element.strip(" ")
+            txt = element + char if txt is "" else txt + "\n\n" + element + char
+
+    print(txt[:-3], end="")
+    
