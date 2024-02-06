3d42f4bf2e3d3b437822793ea650820d2c6ff8fb
Thabang Maphangisane <thabangh647@gmail.com>
2024-02-06 17:57:08 +0200

    9. A square is a rectangle

--- 0x08-python-more_classes/9-main.py
+++ 0x08-python-more_classes/9-main.py
@@ -4,3 +4,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 my_square = Rectangle.square(5)
 print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
 print(my_square)
+
+my_square1 = Rectangle.square(3)
+print("Area: {} - Perimeter: {}".format(my_square1.area(), my_square1.perimeter()))
+print(my_square1)
