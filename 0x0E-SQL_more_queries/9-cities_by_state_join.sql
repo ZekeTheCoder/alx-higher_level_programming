fec05d96e4dd867202471a21924107eeebd522b5
Thabang Maphangisane <thabangh647@gmail.com>
2024-02-14 21:27:53 +0200

    9. Cities by States

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Thsi script lists all cities contained in the database hbtn_0d_usa.
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+where (states.id = cities.state_id)
+ORDER BY cities.id ASC;
