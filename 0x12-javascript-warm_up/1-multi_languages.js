793f5ce05635e7f69f43eb080ca00577cd3d1e0d
Thabang Maphangisane <thabangh647@gmail.com>
2024-03-11 17:23:08 +0200

    1. 3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /*
- * Write a script that prints 3 lines:
+ * This script prints 3 lines:
  * - The first line: “C is fun”
  * - The second line: “Python is cool”
  * - The third line: “JavaScript is amazing”
