abf3aada8d3027105cc22281a17e1c6f0495944a
Thabang Maphangisane <thabangh647@gmail.com>
2024-03-12 21:12:20 +0200

    8. Esrever

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* A function that returns the reversed version of a list: */
+
+exports.esrever = function (list) {
+  const reverseList = [];
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    reverseList.push(list[i]);
+  }
+  return (reverseList);
+};
