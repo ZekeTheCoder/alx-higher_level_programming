91575e93ec445a1794974a81701229f91dee18b8
Thabang Maphangisane <maphangisane@gmail.com>
2024-03-19 01:52:02 -0700

    14. Cities in state

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Contains class definition of City.
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """Represents a city for a MySQL database.
+    __tablename__ (str): The name of the MySQL table to store Cities.
+    id (sqlalchemy.Integer): The city's id.
+    name (sqlalchemy.String): The city's name.
+    state_id (sqlalchemy.Integer): Id of the state to which the city belongs.
+   """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
