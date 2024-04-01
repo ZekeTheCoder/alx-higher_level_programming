267a8087b1a7e92153fbf7d1c21e7cee74adf226
Thabang Maphangisane <thabangh647@gmail.com>
2024-04-01 15:35:34 -0700

    9. My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This Python script takes GitHub credentials (username and personal access token) and uses the GitHub API to display your id.
+This Python script takes GitHub credentials(username and personal access token
+and uses the GitHub API to display your id.
 """
 import requests
 import sys
@@ -10,5 +11,5 @@ from requests.auth import HTTPBasicAuth
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     result = requests.get("https://api.github.com/user", auth=auth)
-    
+
     print(result.json().get("id"))
