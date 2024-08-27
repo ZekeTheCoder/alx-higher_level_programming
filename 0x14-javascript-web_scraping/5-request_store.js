63dc99e6864dbd250787aad7f4bf25bfa68f5c87
Thabang Maphangisane <thabangh647@gmail.com>
2024-08-27 15:28:23 +0200

    5. Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
