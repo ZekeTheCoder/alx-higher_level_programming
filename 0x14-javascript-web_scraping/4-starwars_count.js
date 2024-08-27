4018ebf7197f43c21601df210c592853c135a7aa
Thabang Maphangisane <thabangh647@gmail.com>
2024-08-27 15:13:15 +0200

    4. Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    let moviesCount = 0;
+    const content = JSON.parse(body);
+		const wedgeAntillesUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+    content.results.forEach(film => {
+      if (film.characters.includes(wedgeAntillesUrl)) {
+        moviesCount++;
+      }
+    });
+
+    console.log(moviesCount);
+  }
+});
