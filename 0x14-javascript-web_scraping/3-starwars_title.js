5b878dc97e03bc7fc6b7c799cc540eef2eae5eb1
Thabang Maphangisane <thabangh647@gmail.com>
2024-08-27 14:58:57 +0200

    3. Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const film = JSON.parse(body);
+    console.log(film.title);
+  }
+});
