2840228e1632806ab5b3a96f9740bf2fe483a3c8
Thabang Maphangisane <thabangh647@gmail.com>
2024-08-27 16:21:43 +0200

    4. Star wars Wedge Antilles - character Url and ID

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
 const apiUrl = process.argv[2];
+const characterId = '18'; // Wedge Antilles
+const characterUrl = `https://swapi-api.alx-tools.com/api/people/${characterId}/`;
 
 request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
+    const films = JSON.parse(body).results;
     let moviesCount = 0;
-    const content = JSON.parse(body);
-		const wedgeAntillesUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
 
-    for (const film of content.results) {
-      if (film.characters.includes(wedgeAntillesUrl)) {
-        moviesCount++;
-      }
-    }
+    for (const film of films) {
+      for (const characterUrl of film.characters) {
+        if (characterUrl.includes(characterId)) {
+          moviesCount++;
+          break; // Once we find the character in this film
+        }
+			}}
 
     console.log(moviesCount);
   }
